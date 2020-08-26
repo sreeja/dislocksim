840084c9f3a3e55806e0270c86f400b54324b1f1
Sreeja <sreeja.in@gmail.com>
2020-08-26 18:35:14 +0200

    adding lock configurations - granularity and placement

--- experiment.sh
+++ experiment.sh
@@ -9,3 +9,29 @@ export LOCKTYPE=1
 make dockrun-cent
 # make dockrun-clust
 # make dockrun-dist
+
+
+# auction1
+# granularity 1, type - 1-9
+# granularity 2, type - 1-3
+# 12 configurations
+
+# auction2
+# granularity 1, type - 1-27
+# granularity 2, type - 1-9
+# 36 configurations
+
+# auction3
+# granularity 1, type - 81
+# granularity 2, type - 81
+# granularity 3, type - 27
+# granularity 4, type - 27
+# granularity 5, type - 27
+# granularity 6, type - 27
+# granularity 7, type - 27
+# granularity 8, type - 27
+# granularity 9, type - 9
+# granularity 10, type - 9
+# 342 configurations
+
+# 390 configurations in total
